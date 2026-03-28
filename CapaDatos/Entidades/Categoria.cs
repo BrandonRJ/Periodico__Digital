d2d86e58bfe2162c622078a35f50cfb4bcecc609
Ramses Barboza Cantillano <ramsesbarboza0@gmail.com>
2026-03-28 17:20:57 -0600

    Creacion de clase de categorias

--- CapaDatos/Entidades/Categoria.cs
+++ CapaDatos/Entidades/Categoria.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Periodico__Digital.CapaDatos.Entidades;
+using Periodico_Digital.CapaDatos.Entidades;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
-namespace Periodico__Digital.CapaDatos.Entidades
+namespace Periodico_Digital.CapaDatos.Entidades
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
+        [Column("IdCategoria")]
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre de categoría es obligatorio")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [StringLength(200)]
+        public string Descripcion { get; set; }
+
+        // Relación: Una categoría tiene muchas noticias
+        public virtual ICollection<Noticia> Noticias { get; set; }
     }
 }
