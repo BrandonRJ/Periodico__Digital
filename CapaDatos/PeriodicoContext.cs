c72fd3adb4f9573f4be350121595c29fd49acc72
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-28 17:42:47 -0600

    Creacion calse PeriodicoContext.cs y su logica.

---
+++ CapaDatos/PeriodicoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Periodico_Digital.CapaDatos.Entidades;
+
+namespace Periodico_Digital.CapaDatos
+{
+    public class PeriodicoContext : DbContext
+    {
+        // Este nombre debe estar en tu Web.config
+        public PeriodicoContext() : base("name=PeriodicoConn")
+        {
+        }
+
+        // Tablas del Periódico
+        public DbSet<Noticia> Noticias { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
