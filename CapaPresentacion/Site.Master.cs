09fbcd03d049d4ad150137a663f2faa4e30be1c0
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-28 21:00:49 -0600

    Creacion de pagina Master Page.

--- CapaPresentacion/Site.Master.cs
+++ CapaPresentacion/Site.Master.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Periodico__Digital
+namespace Periodico__Digital.CapaPresentacion
 {
-    public partial class SiteMaster : MasterPage
+    public partial class Site : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
