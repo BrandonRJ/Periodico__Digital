f2ef93e2aa359c30f43c80c6271c7affa90f8a36
Brandon Daniel Robles Jimenez <brandon-95cr@hotmail.com>
2026-03-29 01:42:28 -0600

    Creacion de clase AutorNegocio.cs  y CategoriaNegocio.cs

---
+++ CapaNegocio/AutorNegocio.cs
@@ -0,0 +1,63 @@
+﻿using Periodico_Digital.CapaDatos;
+using Periodico_Digital.CapaDatos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity; // <--- AGREGAR ESTO PARA EL .INCLUDE
+
+namespace Periodico_Digital.CapaNegocio
+{
+    public class AutorNegocio
+    {
+        // 1. LISTAR CON REPORTE (Punto 3.b)
+        public List<Autor> ListarAutores()
+        {
+            using (var db = new PeriodicoContext())
+            {
+                // Agregamos .Include para que el GridView pueda contar las noticias
+                return db.Autores.Include(a => a.Noticias).ToList();
+            }
+        }
+
+        // 2. INSERTAR AUTOR
+        public bool Insertar(string nombre)
+        {
+            using (var db = new PeriodicoContext())
+            {
+                // Si su entidad Autor tiene Email, recuerden agregarlo aquí
+                db.Autores.Add(new Autor { Nombre = nombre });
+                return db.SaveChanges() > 0;
+            }
+        }
+
+        // 3. ELIMINAR AUTOR (Opcional pero recomendado para "Gestionar")
+        public bool Eliminar(int id)
+        {
+            using (var db = new PeriodicoContext())
+            {
+                var autor = db.Autores.Find(id);
+                if (autor != null)
+                {
+                    db.Autores.Remove(autor);
+                    return db.SaveChanges() > 0;
+                }
+                return false;
+            }
+        }
+
+        internal object ObtenerReporteAutores()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool RegistrarAutor(string nombre, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool EliminarAutor(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CapaNegocio/CategoriaNegocio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Periodico_Digital.CapaDatos;
+using Periodico_Digital.CapaDatos.Entidades;
+using System.Data.Entity;
+
+namespace Periodico_Digital.CapaNegocio
+{
+    public class CategoriaNegocio
+    {
+        // 1. LISTAR  noticias para el conteo)
+        public List<Categoria> ListarCategorias()
+        {
+            using (var db = new PeriodicoContext())
+            {
+                // El .Include("Noticias") es lo que hace que funcione el campo 'TotalNoticias'
+                return db.Categorias.Include(c => c.Noticias).ToList();
+            }
+        }
+
+        // 2. INSERTAR 
+        public bool Insertar(string descripcion)
+        {
+            using (var db = new PeriodicoContext())
+            {
+                db.Categorias.Add(new Categoria { Descripcion = descripcion });
+                return db.SaveChanges() > 0;
+            }
+        }
+
+        // 3. ELIMINAR (Para completar la gestión)
+        public bool Eliminar(int id)
+        {
+            using (var db = new PeriodicoContext())
+            {
+                var cat = db.Categorias.Find(id);
+                if (cat != null)
+                {
+                    db.Categorias.Remove(cat);
+                    return db.SaveChanges() > 0;
+                }
+                return false;
+            }
+        }
+    }
+}
