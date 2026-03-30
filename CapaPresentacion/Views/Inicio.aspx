f5b7e3d36b5e5e5930aa91d6535b5f36ef7e8ff7
Ramses Barboza Cantillano <ramsesbarboza0@gmail.com>
2026-03-29 21:52:27 -0600

    Creacion de Inicio.aspx y codebehind, estilos para inicio y arreglos en site.master

---
+++ CapaPresentacion/Views/Inicio.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/CapaPresentacion/Site.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="Periodico__Digital.CapaPresentacion.Views.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="contenido-wrapper">
+        <h2>Pagina de inicio</h2>
+        <br />
+        <div class="ddl-group">
+            <asp:DropDownList ID="ddlAutor" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlAutor_SelectedIndexChanged">
+                
+            </asp:DropDownList>
+
+            <asp:DropDownList ID="ddlCategoria" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCategoria_SelectedIndexChanged">
+                
+            </asp:DropDownList>
+        </div>
+
+
+        <div class="contenedor-seccion">
+            <asp:GridView ID="gvNoticias" runat="server" AutoGenerateColumns="false" CssClass="tabla-inventario">
+                <HeaderStyle CssClass="tabla-header" />
+                <AlternatingRowStyle CssClass="tabla-row-alt" />
+                <Columns>
+                    <asp:BoundField DataField="Titulo" HeaderText="Título" />
+                    <asp:BoundField DataField="Autor.Nombre" HeaderText="Autor" />
+                    <asp:BoundField DataField="Categoria.Descripcion" HeaderText="Categoría" />
+                    <asp:BoundField DataField="FechaPublicacion" HeaderText="Fecha" DataFormatString="{0:dd/MM/yyyy}" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ CapaPresentacion/Views/Inicio.aspx.cs
@@ -0,0 +1,98 @@
+﻿using Periodico_Digital.CapaDatos;
+using Periodico_Digital.CapaDatos.Entidades;
+using Periodico_Digital.CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace Periodico__Digital.CapaPresentacion.Views
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                CargarCombos();
+                cargarGrid();
+            }
+            
+        }
+
+        // Cargar tabla
+        private void cargarGrid()
+        {
+            try
+            {
+                var service = new NoticiaService();
+
+                gvNoticias.DataSource = service.ListarPublicadas();
+                gvNoticias.DataBind();
+            }
+            catch (Exception ex)
+            {
+
+                MostrarAlerta("Error al cargar la lista: " + ex.Message);
+            }
+        }
+
+        //Datos para las ddl
+        private void CargarCombos()
+        {
+            using (var db = new PeriodicoContext())
+            {
+                // Cargar Autores
+                var autores = db.Autores.ToList();
+                ddlAutor.DataSource = autores;
+                ddlAutor.DataTextField = "Nombre";
+                ddlAutor.DataValueField = "Id";
+                ddlAutor.DataBind();
+                ddlAutor.Items.Insert(0, new ListItem("-- Filtrar por Autores --", ""));
+
+                // Cargar Categorías
+                var categorias = db.Categorias.ToList();
+                ddlCategoria.DataSource = categorias;
+                ddlCategoria.DataTextField = "Descripcion";
+                ddlCategoria.DataValueField = "Id";
+                ddlCategoria.DataBind();
+                ddlCategoria.Items.Insert(0, new ListItem("-- Filtrar por categoria --", ""));
+            }
+        }
+
+        private void CargarNoticias(int? autorId, int? categoriaId) 
+        {
+            var service = new NoticiaService();
+            gvNoticias.DataSource = service.ListarPublicadas(autorId, categoriaId);
+            gvNoticias.DataBind();
+        }
+
+
+        //Filtros para los ddl
+        private void AplicarFlitros() 
+        {
+            int? autorId = string.IsNullOrEmpty(ddlAutor.SelectedValue) ? null : (int?)int.Parse(ddlAutor.SelectedValue);
+            int? categoriaId = string.IsNullOrEmpty(ddlCategoria.SelectedValue) ? null : (int?)int.Parse(ddlCategoria.SelectedValue);
+            CargarNoticias(autorId, categoriaId);
+        }
+
+        protected void ddlAutor_SelectedIndexChanged(object sender, EventArgs e) 
+        {
+            AplicarFlitros();
+        }
+
+        protected void ddlCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AplicarFlitros();
+        }
+
+        private void MostrarAlerta(string mensaje)
+        {
+            string script = $"alert('{mensaje.Replace("'", "\\'")}');";
+            ScriptManager.RegisterStartupScript(this, GetType(), "alert", script, true);
+        }
+    }
+}
